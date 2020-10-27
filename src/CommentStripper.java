c4a7e96ec8a0923b5b585a5960b7a9077fb12dc9
Jimmy Martin <60280370+ZealithDjo@users.noreply.github.com>
2020-10-27 17:06:48 -0400

    Add files via upload

---
+++ src/CommentStripper.java
@@ -0,0 +1,47 @@
+
+public class CommentStripper
+{
+
+    public static void main(String[] args)
+    {
+        final int CODE  = 0;   // parsing normal code
+        final int SLASH = 1;   // found a leading '/'
+        final int BLOCK = 2;   // in a block comment
+        final int LINE  = 3;   // in a line comment
+        final int STAR  = 4;   // found a trailing * in a block comment
+        final int QUOTE = 5;
+
+        int state = CODE;      // current state
+
+        while(!StdIn.isEmpty()) {
+            char c = StdIn.readChar();
+            switch(state) {
+                case CODE:  if      (c == '/')  { state = SLASH;                           }
+                            else if (c == '"')  { state = QUOTE;     StdOut.print('"');    } 
+                            else                { StdOut.print(c);                           }
+                            break;
+
+                case SLASH: if      (c == '*')  { state = BLOCK;                           }
+                            else if (c == '/')  { state = LINE;                            }
+                            else                { state = CODE; StdOut.print("/" + c);      }
+                            break;
+
+                case BLOCK: if      (c == '*')  { state = STAR;                            }
+                            break;
+
+                case STAR:  if      (c == '/')  { state = CODE; StdOut.print(" ");     }
+                            else if (c == '*')  { state = STAR;                            }
+                            else                { state = BLOCK;                           }
+                            break;
+
+                case LINE:  if      (c == '\n') { state = CODE; StdOut.println();      }
+                            break;
+                            
+                case QUOTE: if      (c == '"') { state = CODE;   StdOut.print('"');     }
+                            else { StdOut.print(c);         }
+                break;
+             }
+        }
+    }
+
+}
---
+++ src/StripperTest.java
@@ -0,0 +1,20 @@
+
+public class StripperTest
+{
+
+    public static void main(String[] args)
+    {
+        //YOU SHOULDNT SEE THIS
+        
+        System.out.println("Standard Test");
+        
+        /**
+         * YOU SHOULDNT SEE THIS
+         */
+        
+        System.out.println("the following should stay://YOU SHOULD SEE THIS(1/2)");
+        
+        System.out.println("This should stay too: /** YOU SHOULD SEE THIS(2/2) */");
+    }
+
+}
